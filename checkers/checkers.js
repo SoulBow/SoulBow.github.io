e2a4c77f04de264873d4c25ee0f6130f737f9ce2
soulbow10@gmail.com <soulbow10@gmail.com>
2023-11-26 21:17:03 -0600

    WebPage

---
+++ checkers/checkers.js
@@ -0,0 +1,85 @@
+function tile (element) {
+if (element.style.background=="red" || element.style.background=="black") {
+element.style.background="deeppink";
+element.style.border="deeppink";}
+
+else if (element.style.background=="deeppink" && document.getElementById("button").style.background=="red") {
+element.style.background="red";
+element.style.border="solid black"}
+
+else if (element.style.background=="deeppink" && document.getElementById("button").style.background=="black"){
+element.style.background="black";
+element.style.border="solid black";}}
+
+function color(element) {
+if (element.style.background=="red") {element.style.background="black";
+  element.style.color="red";}
+else {element.style.background="red";
+element.style.color="black";}}
+
+function reset() {
+document.getElementById("id1").style.background="red";
+document.getElementById("id1").style.border="solid black";
+document.getElementById("id2").style.background="red";
+document.getElementById("id2").style.border="solid black";
+document.getElementById("id3").style.background="red";
+document.getElementById("id3").style.border="solid black";
+document.getElementById("id4").style.background="red";
+document.getElementById("id4").style.border="solid black";
+document.getElementById("id5").style.background="red";
+document.getElementById("id5").style.border="solid black";
+document.getElementById("id6").style.background="red";
+document.getElementById("id6").style.border="solid black";
+document.getElementById("id7").style.background="red";
+document.getElementById("id7").style.border="solid black";
+document.getElementById("id8").style.background="red";
+document.getElementById("id8").style.border="solid black";
+document.getElementById("id9").style.background="red";
+document.getElementById("id9").style.border="solid black";
+document.getElementById("id10").style.background="red";
+document.getElementById("id10").style.border="solid black";
+document.getElementById("id11").style.background="red";
+document.getElementById("id11").style.border="solid black";
+document.getElementById("id12").style.background="red";
+document.getElementById("id12").style.border="solid black";
+document.getElementById("id21").style.background="black";
+document.getElementById("id21").style.border="solid black";
+document.getElementById("id22").style.background="black";
+document.getElementById("id22").style.border="solid black";
+document.getElementById("id23").style.background="black";
+document.getElementById("id23").style.border="solid black";
+document.getElementById("id24").style.background="black";
+document.getElementById("id24").style.border="solid black";
+document.getElementById("id25").style.background="black";
+document.getElementById("id25").style.border="solid black";
+document.getElementById("id26").style.background="black";
+document.getElementById("id26").style.border="solid black";
+document.getElementById("id27").style.background="black";
+document.getElementById("id27").style.border="solid black";
+document.getElementById("id28").style.background="black";
+document.getElementById("id28").style.border="solid black";
+document.getElementById("id29").style.background="black";
+document.getElementById("id29").style.border="solid black";
+document.getElementById("id30").style.background="black";
+document.getElementById("id30").style.border="solid black";
+document.getElementById("id31").style.background="black";
+document.getElementById("id31").style.border="solid black";
+document.getElementById("id32").style.background="black";
+document.getElementById("id32").style.border="solid black";
+document.getElementById("id13").style.background="deeppink";
+document.getElementById("id13").style.border="deeppink";
+document.getElementById("id14").style.background="deeppink";
+document.getElementById("id14").style.border="deeppink";
+document.getElementById("id15").style.background="deeppink";
+document.getElementById("id15").style.border="deeppink";
+document.getElementById("id16").style.background="deeppink";
+document.getElementById("id16").style.border="deeppink";
+document.getElementById("id17").style.background="deeppink";
+document.getElementById("id17").style.border="deeppink";
+document.getElementById("id18").style.background="deeppink";
+document.getElementById("id18").style.border="deeppink";
+document.getElementById("id19").style.background="deeppink";
+document.getElementById("id19").style.border="deeppink";
+document.getElementById("id20").style.background="deeppink";
+document.getElementById("id20").style.border="deeppink";
+}
