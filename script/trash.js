c6ed334372678030c6ad0ac73d77be760472735b
SoulBow <126333544+SoulBow@users.noreply.github.com>
2024-01-27 03:08:22 -0600

    Update trash.js

--- script/trash.js
+++ script/trash.js
@@ -1,5 +1,255 @@
+var one=document.getElementById("idH1");
+var two=document.getElementById("idF1");
+var three=document.getElementById("idH2");
+var four=document.getElementById("idF2");
+var five=document.getElementById("idH3");
+var six=document.getElementById("idF3");
+var seven=document.getElementById("idH4");
+var eight=document.getElementById("idF4");
+var nine=document.getElementById("idH5");
+var ten=document.getElementById("idF5");
+var eleven=document.getElementById("idH6");
+var twelve=document.getElementById("idF6");
+var thirteen=document.getElementById("idH7");
+var fourteen=document.getElementById("idF7");
+var fifteen=document.getElementById("idH8");
+var sixteen=document.getElementById("idF8");
+var seventeen=document.getElementById("idH9");
+var eighteen=document.getElementById("idF9");
+var nineteen=document.getElementById("idH10");
+var twenty=document.getElementById("idF10");
+var twentyone=document.getElementById("idH11");
+var twentytwo=document.getElementById("idF11");
+var twentythree=document.getElementById("idH12");
+var twentyfour=document.getElementById("idF12");
+var twentyfive=document.getElementById("idH13");
+var twentysix=document.getElementById("idF13");
+var twentyseven=document.getElementById("idH14");
+var twentyeight=document.getElementById("idF14");
+var twentynine=document.getElementById("idH15");
+var thirdy=document.getElementById("idF15");
+var thirdyone=document.getElementById("idH16");
+var thirdytwo=document.getElementById("idF16");
+var thirdythree=document.getElementById("idH17");
+var thirdyfour=document.getElementById("idF17");
+var thirdyfive=document.getElementById("idH18");
+var thirdysix=document.getElementById("idF18");
+var thirdyseven=document.getElementById("idH19");
+var thirdyeight=document.getElementById("idF19");
+var thirdynine=document.getElementById("idH20");
+var fourty=document.getElementById("idF20");
+var fourtyone=document.getElementById("idH21");
+var fourtytwo=document.getElementById("idF21");
+var fourtythree=document.getElementById("idH22");
+var fourtyfour=document.getElementById("idF22");
+var fourtyfive=document.getElementById("idH23");
+var fourtysix=document.getElementById("idF23");
+var fourtyseven=document.getElementById("idH24");
+var fourtyeight=document.getElementById("idF24");
+var fourtynine=document.getElementById("idH25");
+var fifty=document.getElementById("idF25");
+var fiftyone=document.getElementById("idH26");
+var fiftytwo=document.getElementById("idF26");
+var fiftythree=document.getElementById("idH27");
+var fiftyfour=document.getElementById("idF27");
+var fiftyfive=document.getElementById("idH28");
+var fiftysix=document.getElementById("idF28");
+var fiftyseven=document.getElementById("idH29");
+var fiftyeight=document.getElementById("idF29");
+var fiftynine=document.getElementById("idH30");
+var sixty=document.getElementById("idF30");
+var sixtyone=document.getElementById("idH31");
+var sixtytwo=document.getElementById("idF31");
+var sixtythree=document.getElementById("idH32");
+var sixtyfour=document.getElementById("idF32");
 function trash () {
-if (document.getElementById("trash").style.background=="red") {document.getElementById("trash").style.background="blue";
+if (one.style.background=="red") {
+one.style.background="gold";
+one.innerHTML="";}
+else if (two.style.background=="red") {
+two.style.background="silver";
+two.innerHTML="";}
+else if (three.style.background=="red") {
+three.style.background="gold";
+three.innerHTML="";}
+else if (four.style.background=="red") {
+four.style.background="silver";
+four.innerHTML="";}
+else if (five.style.background=="red") {
+five.style.background="gold";
+five.innerHTML="";}
+else if (six.style.background=="red") {
+six.style.background="silver";
+six.innerHTML="";}
+else if (seven.style.background=="red") {
+seven.style.background="silver";
+seven.innerHTML="";}
+else if (eight.style.background=="red") {
+eight.style.background="gold";
+eight.innerHTML="";}
+else if (nine.style.background=="red") {
+nine.style.background="silver";
+nine.innerHTML="";}
+else if (ten.style.background=="red") {
+ten.style.background="gold";
+ten.innerHTML="";}
+else if (eleven.style.background=="red") {
+eleven.style.background="silver";
+eleven.innerHTML="";}
+else if (twelve.style.background=="red") {
+twelve.style.background="gold";
+twelve.innerHTML="";}
+else if (thirteen.style.background=="red") {
+thirteen.style.background="silver";
+thirtewn.innerHTML="";}
+else if (fourteen.style.background=="red") {
+fourteen.style.background="gold";
+fourteen.innerHTML="";}
+else if (fifteen.style.background=="red") {
+fifteen.style.background="gold";
+fifteen.innerHTML="";}
+else if (sixteen.style.background=="red") {
+sixteen.style.background="silver";
+sixteen.innerHTML="";}
+else if (seventeen.style.background=="red") {
+seventeen.style.background="gold";
+seventeen.innerHTML="";}
+else if (eighteen.style.background=="red") {
+eighteen.style.background="silver";
+eighteen.innerHTML="";}
+else if (nineteen.style.background=="red") {
+nineteen.style.background="gold";
+nineteen.innerHTML="";}
+else if (twenty.style.background=="red") {
+twenty.style.background="silver";
+twenty.innerHTML="";}
+else if (twentyone.style.background=="red") {
+twwntyone.style.background="gold";
+twentyone.innerHTML="";}
+else if (twentytwo.style.background=="red") {
+twwntytwo.style.background="silver";
+twentytwo.innerHTML="";}
+else if (twentythree.style.background=="red") {
+twentythree.style.background="silver";
+twentythree.innerHTML="";}
+else if (twentyfour.style.background=="red") {
+twentyfour.style.background="gold";
+twentyfour.innerHTML="";}
+else if (twentyfive.style.background=="red") {
+twentyfive.style.background="silver";
+twentyfive.innerHTML="";}
+else if (twentysix.style.background=="red") {
+twentysix.style.background="gold";
+twentysix.innerHTML="";}
+else if (twentyseven.style.background=="red") {
+twwntyseven.style.background="silver";
+twentyseven.innerHTML="";}
+else if (twentyeight.style.background=="red") {
+twentyeight.style.background="gold";
+twentyeight.innerHTML="";}
+else if (twentynine.style.background=="red") {
+twentynine.style.background="silver";
+twentynine.innerHTML="";}
+else if (thirdy.style.background=="red") {
+thirdy.style.background="gold";
+thirdy.innerHTML="";}
+else if (thirdyone.style.background=="red") {
+thirdyone.style.background="gold";
+thirdyone.innerHTML="";}
+else if (thirdytwo.style.background=="red") {
+thirdytwo.style.background="silver";
+thirdytwo.innerHTML="";}
+else if (thirdythree.style.background=="red") {
+thirdythree.style.background="gold";
+thirdythree.innerHTML="";}
+else if (thirdyfive.style.background=="red") {
+thirdyfive.style.background="silver";
+thirdyfive.innerHTML="";}
+else if (thirdysix.style.background=="red") {
+thirdysix.style.background="gold";
+thirdysix.innerHTML="";}
+else if (thirdyseven.style.background=="red") {
+thirdyseven.style.background="silver";
+thirdyseven.innerHTML="";}
+else if (thirdyeight.style.background=="red") {
+thirdyeight.style.background="gold";
+thirdyeight.innerHTML="";}
+else if (thirdynine.style.background=="red") {
+thirdynine.style.background="silver";
+thirdynine.innerHTML="";}
+else if (fourty.style.background=="red") {
+fourty.style.background="silver";
+fourty.innerHTML="";}
+else if (fourtyone.style.background=="red") {
+fourtyone.style.background="gold";
+fourtyone.innerHTML="";}
+else if (fourtytwo.style.background=="red") {
+fourtytwo.style.background="silver";
+fourtytwo.innerHTML="";}
+else if (fourtythree.style.background=="red") {
+fourtythree.style.background="gold";
+fourtythree.innerHTML="";}
+else if (fourtyfive.style.background=="red") {
+fourtyfive.style.background="silver";
+fourtyfive.innerHTML="";}
+else if (fourtysix.style.background=="red") {
+fourtysix.style.background="gold";
+fourtysix.innerHTML="";}
+else if (fourtyseven.style.background=="red") {
+fourtyseven.style.background="silver";
+fourtyseven.innerHTML="";}
+else if (fourtyeight.style.background=="red") {
+fourtyeight.style.background="gold";
+fourtyeight.innerHTML="";}
+else if (fourtynine.style.background=="red") {
+fourtynine.style.background="gold";
+fourtynine.innerHTML="";}
+else if (fifty.style.background=="red") {
+fifty.style.background="silver";
+fifty.innerHTML="";}
+else if (fiftyone.style.background=="red") {
+fiftyone.style.background="gold";
+fiftyone.innerHTML="";}
+else if (fiftytwo.style.background=="red") {
+fiftytwo.style.background="silver";
+fiftytwo.innerHTML="";}
+else if (fiftythree.style.background=="red") {
+fiftythree.style.background="gold";
+fiftythree.innerHTML="";}
+else if (fiftyfour.style.background=="red") {
+fiftyfour.style.background="silver";
+fiftyfour.innerHTML="";}
+else if (fiftyfive.style.background=="red") {
+fiftyfive.style.background="gold";
+fiftyfive.innerHTML="";}
+else if (fiftysix.style.background=="red") {
+fiftysix.style.background="silver";
+fiftysix.innerHTML="";}
+else if (fiftyseven.style.background=="red") {
+fiftyseven.style.background="silver";
+fiftyseven.innerHTML="";}
+else if (fiftyeight.style.background=="red") {
+fiftyeight.style.background="gold";
+fiftyeight.innerHTML="";}
+else if (fiftynine.style.background=="red") {
+fiftynine.style.background="silver";
+fiftynine.innerHTML="";}
+else if (sixty.style.background=="red") {
+sixty.style.background="gold";
+sixty.innerHTML="";}
+else if (sixtyone.style.background=="red") {
+sixtyone.style.background="silver";
+sixtyone.innerHTML="";}
+else if (sixtytwo.style.background=="red") {
+sixtytwo.style.background="gold";
+sixtytwo.innerHTML="";}
+else if (sixtythree.style.background=="red") {
+sixtythree.style.background="silver";
+sixtythree.innerHTML="";}
+else if (sixtyfour.style.background=="red") {
+sixtyfour.style.background="gold";
+sixtyfour.innerHTML="";}
+else if (document.getElementById("trash").style.background=="red") {document.getElementById("trash").style.background="blue";
 document.getElementById("whiteBishop").style.background="grey";
 document.getElementById("whiteKnight").style.background="grey";
 document.getElementById("whiteRook").style.background="grey";
