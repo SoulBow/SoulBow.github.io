38245a58408f8cdc2d28ca6689d9739da6b21ee1
SoulBow <126333544+SoulBow@users.noreply.github.com>
2024-02-20 15:03:00 -0600

    Add files via upload

---
+++ tac/tac.js
@@ -0,0 +1,55 @@
+const idp1= document.getElementById("p1");
+const idp2= document.getElementById("p2");
+const idp3= document.getElementById("p3");
+const idp4= document.getElementById("p4");
+const idp5= document.getElementById("p5");
+const idp6= document.getElementById("p6");
+const idp7= document.getElementById("p7");
+const idp8= document.getElementById("p8");
+const idp9= document.getElementById("p9");
+function tac1 () {
+if (idp1.innerHTML=="x"){idp1.innerHTML="o";}
+else if (idp1.innerHTML=="o"){idp1.innerHTML="";}
+else {idp1.innerHTML="x"}}
+function tac2 () {
+if (idp2.innerHTML=="x"){idp2.innerHTML="o";}
+else if (idp2.innerHTML=="o"){idp2.innerHTML="";}
+else {idp2.innerHTML="x"}}
+function tac3 () {
+if (idp3.innerHTML=="x"){idp3.innerHTML="o";}
+else if (idp3.innerHTML=="o"){idp3.innerHTML="";}
+else {idp3.innerHTML="x"}}
+function tac4 () {
+if (idp4.innerHTML=="x"){idp4.innerHTML="o";}
+else if (idp4.innerHTML=="o"){idp4.innerHTML="";}
+else {idp4.innerHTML="x"}}
+function tac5 () {
+if (idp5.innerHTML=="x"){idp5.innerHTML="o";}
+else if (idp5.innerHTML=="o"){idp5.innerHTML="";}
+else {idp5.innerHTML="x"}}
+function tac6 () {
+if (idp6.innerHTML=="x"){idp6.innerHTML="o";}
+else if (idp6.innerHTML=="o"){idp6.innerHTML="";}
+else {idp6.innerHTML="x"}}
+function tac7 () {
+if (idp7.innerHTML=="x"){idp7.innerHTML="o";}
+else if (idp7.innerHTML=="o"){idp7.innerHTML="";}
+else {idp7.innerHTML="x"}}
+function tac8 () {
+if (idp8.innerHTML=="x"){idp8.innerHTML="o";}
+else if (idp8.innerHTML=="o"){idp8.innerHTML="";}
+else {idp8.innerHTML="x"}}
+function tac9 () {
+if (idp9.innerHTML=="x"){idp9.innerHTML="o";}
+else if (idp9.innerHTML=="o"){idp9.innerHTML="";}
+else {idp9.innerHTML="x"}}
+function reset() {
+idp1.innerHTML="";
+idp2.innerHTML="";
+idp3.innerHTML="";
+idp4.innerHTML="";
+idp5.innerHTML="";
+idp6.innerHTML="";
+idp7.innerHTML="";
+idp8.innerHTML="";
+idp9.innerHTML="";}
