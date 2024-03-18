0d6a5358e6abd092bdf08797ba705fa38948c51e
SoulBow <126333544+SoulBow@users.noreply.github.com>
2024-03-17 21:10:01 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
 const serverOutput= document.getElementById("output");
 const serverInput= document.getElementById("input");
-var http= require('http://localhost:6700/index.html');
+var http= require('https://soulbow.github.io/indexComment.html');
 http.createServer= (function(req, res){
 res.writehead(200, {'content-type' : 'text/plain'});
-res.end();}).listen("6700");
+res.end();}).listen("http");
 function postIt(){serverOutput.innerHTML= "Diary Date X: " + serverInput.value;
 serverInput="";}
