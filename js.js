9b4bd88450a46040a9c012d3ea245a3e12021913
SoulBow <126333544+SoulBow@users.noreply.github.com>
2024-03-17 20:52:56 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,8 @@
-test
+const serverOutput= document.getElementById("output");
+const serverInput= document.getElementById("input");
+var http= require('http://localhost:6700/index.html');
+http.createServer= (function(req, res){
+res.writehead(200, {'content-type' : 'text/plain'});
+res.end();}).listen("6700");
+function postIt(){serverOutput.innerHTML= "Diary Date X: " + serverInput.value;
+serverInput="";}
